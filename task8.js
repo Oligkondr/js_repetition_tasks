8e24e5fffb9d04b8957dc842b0665a5a33e3e921
Oligkondr <oligkondr@mail.ru>
2023-12-14 13:46:11 +0300

    Done task8 (another variant)

--- task8.js
+++ task8.js
@@ -1,10 +1,6 @@
 const sum = (num1, num2, num3, num4, num5, num6, num7, num8, num9, num10) => {
-    let ans = 0;
     const allNums = [num1, num2, num3, num4, num5, num6, num7, num8, num9, num10];
-    for (const num of allNums) {
-        ans += num;
-    }
-    return ans;
+    return (allNums[0] + allNums[allNums.length - 1]) * (allNums.length / 2);
 };
 
 console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
