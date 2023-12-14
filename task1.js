a3c23b936f7124b8c2d81c333d391a6d4f7a3a4e
Oligkondr <oligkondr@mail.ru>
2023-12-14 13:14:03 +0300

    Done task1

---
+++ task1.js
@@ -0,0 +1,6 @@
+let temperature = 15;
+let value = 10;
+let lang = 'JS';
+let node = 'Node';
+const output = (x, y, z, w) => `Мне нравится изучать ${z} и ${w}!\nСегодня температура ${x} С, а завтра похолодает до (${x} - 3.1) - 11.9 С.\n${y} плюс ${y}, получится 20`;
+console.log(output(temperature, value, lang, node));
